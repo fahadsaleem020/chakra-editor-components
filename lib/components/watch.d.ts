2df53c2f162dd21c13ba1f43c6ea9ef573309af3
palari <fahadsaleem020@gmail.com>
2023-03-12 00:35:08 +0500

    fix: updated deps
    updated deps and missing build

--- lib/components/watch.d.ts
+++ lib/components/watch.d.ts
@@ -1,4 +1,3 @@
 import { FC } from "react";
-import { WatchProps, LookupProps } from "@chakra-editor/types";
+import { WatchProps } from "@chakra-editor/types";
 export declare const Watch: FC<WatchProps>;
-export declare const Lookup: (props: LookupProps) => any;
